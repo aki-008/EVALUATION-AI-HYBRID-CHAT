d7fd328eee16452dc7ed4c591c88e0673a63b4c3
SHASWATSINGH3101 <shaswatsingh910@gmail.com>
2025-10-23 19:22:08 +0000

    redis

--- scripts/embeddings_cache.py
+++ scripts/embeddings_cache.py
@@ -3,6 +3,7 @@ import hashlib
 import pickle
 import os
 
+
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 REDIS_DB = int(os.getenv("REDIS_DB", 0))
